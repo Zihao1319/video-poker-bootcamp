e700fda652416b72fe698c181a13b8c94baeec98
Zihao1319 <zihao1319@gmail.com>
2022-04-06 00:24:30 +0800

    psedocode

---
+++ project1.js
@@ -0,0 +1,11 @@
+// game design put here
+
+// 1) At the beginning, all the cards are flipped backwards. Deal/draw button on bottom right
+// 2) Once the player click on the deal button, the cards will be shown. The deal button will also change to draw button
+// 3) The user can choose to hold the card by clicking the card, once clicked, it will become bigger and a held sign will be shown above, if the click again, it will revert to original and the held sign will be gone
+// 4) When the user click the draw button, it will draw the remaining the cards that is not held, and display either winning or losing
+// 5) at the same time, the points will either increase or decrease
+// 5) at the same time, draw button changes to deal button again
+// 6) User click the deal button and the game will restart again
+
+//function to
